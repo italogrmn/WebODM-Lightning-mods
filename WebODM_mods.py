c2ec19bc3363624529b69d5e83b028e6b60d61e7
italogermano <italo.mortari@enovaenergia.com.br>
2023-11-08 13:20:24 -0300

    v.1.0 stable

--- WebODM_mods.py
+++ WebODM_mods.py
@@ -11,24 +11,24 @@ def dispath():
     user_profile = os.environ['USERPROFILE']
 
     #pastas do webodm
-    webodm_path = r'C:\Users' + "\\" + user_profile + "\\" + r'AppData\Local\Programs\WebODM Lightning'
-    projects_path = user_profile + r'\resources\app\apps\WebODM\app\api'
-    potree_build_path = user_profile + r'\resources\app\apps\WebODM\build\static\app\js\vendor\potree\build\potree'
-    potree_app_path = user_profile + r'\resources\app\apps\WebODM\app\static\app\js\vendor\potree\build\potree'            
+    webodm_path = user_profile + r'\AppData\Local\Programs\WebODM Lightning'
+    projects_path = webodm_path + r'\resources\app\apps\WebODM\app\api'
+    potree_build_path = webodm_path + r'\resources\app\apps\WebODM\build\static\app\js\vendor\potree\build\potree'
+    potree_app_path = webodm_path + r'\resources\app\apps\WebODM\app\static\app\js\vendor\potree\build\potree'            
 
-    if os.path.exists(r'C:\Plugin Revit'):
+    if os.path.exists(webodm_path):
         print('O WebODM foi encontrado. Iniciando cópia...\n\n')
 
         #projects.py
-        shutil.move(package_dir + r'\projects.py', projects_path)
+        shutil.move(package_dir + r'\projects.py', projects_path + r'\projects.py')
         
         #potree mods em build
-        shutil.move(package_dir + r'\potree.js', potree_build_path)
-        shutil.move(package_dir + r'\sidebar.html', potree_build_path)
+        shutil.copy2(package_dir + r'\potree.js', potree_build_path + r'\potree.js')
+        shutil.copy2(package_dir + r'\sidebar.html', potree_build_path + r'\sidebar.html')
         
         #potree mods em app
-        shutil.move(package_dir + r'\potree.js', potree_app_path)
-        shutil.move(package_dir + r'\sidebar.html', potree_app_path)
+        shutil.move(package_dir + r'\potree.js', potree_app_path + r'\potree.js')
+        shutil.move(package_dir + r'\sidebar.html', potree_app_path + r'\sidebar.html')
         
         return 1
     
